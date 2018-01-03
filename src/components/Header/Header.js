d8467220d23be4d118272917166ac45ac80d101b
= <webdev.jameslemire@gmail.com>
2018-01-03 11:08:48 -0700

    JL phase-1 - Code updates.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Header.css';
 
+import iconPng from '../../assets/icon.png';
+
 export default () => (
   <section className="Header__parent">
+    <img src={ iconPng } />
     <span>Header</span>
   </section>
 )
